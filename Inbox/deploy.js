a2e43dda56bb782984125034d577fa0d1c94daa0
Nischith-Shadagopan-M-N <cs18b102@smail.iitm.ac.in>
2022-07-28 10:00:47 +0530

    completed section 2

---
+++ Inbox/deploy.js
@@ -0,0 +1,25 @@
+const HDWalletProvider = require('@truffle/hdwallet-provider')
+const Web3 = require('web3');
+const { interface, bytecode } = require('./compile');
+
+const provider = new HDWalletProvider(
+    'polar infant sound liquid faint fix sheriff retire say obscure submit soldier', 
+    'https://rinkeby.infura.io/v3/1a4af15237cf43089080644d2da1c1cd'
+);
+
+const web3 = new Web3(provider)
+
+const deploy = async ()=>{
+    // Get a list of all accounts
+    const accounts = await web3.eth.getAccounts();
+    console.log('Attempting to deploy from account', accounts[0])
+    // Use one of the accounts to deploy the contract
+    const result = await new web3.eth.Contract(JSON.parse(interface))
+        .deploy({data:bytecode, arguments:['hello there']})
+        .send({from:accounts[0], gas:'1000000'});
+
+    console.log('Contract deployed to', result.options.address);
+
+    provider.engine.stop();
+};
+deploy();
